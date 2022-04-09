fda7b9f723aef6205ecd5898271889441eead9b7
Bunyad <the.bunyad@gmail.com>
2022-04-09 17:38:56 +0400

    added a label

--- Lesson 1.1/ViewController.swift
+++ Lesson 1.1/ViewController.swift
@@ -12,6 +12,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .darkGray
+        let lbl = UILabel()
+        lbl.frame = CGRect.init(x: 140, y: 400, width: 150, height: 30)
+        lbl.text = "Testing.."
+        lbl.textColor = .orange
+        lbl.textAlignment = .center
+        self.view.addSubview(lbl)
     }
 
 
