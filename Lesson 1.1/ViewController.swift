0cbfa9b3d1db7ad6e7767c07d4ed347328d891de
Bunyad <the.bunyad@gmail.com>
2022-04-09 18:39:35 +0400

    hmm

--- Lesson 1.1/ViewController.swift
+++ Lesson 1.1/ViewController.swift
@@ -16,19 +16,21 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         self.view.backgroundColor = .darkGray
-        
+        let font: UIFont = UIFont.systemFont(ofSize: 30)
        
         lbl.frame = CGRect.init(x: 140, y: 400, width: 150, height: 30)
         lbl.text = "Testing.."
         lbl.textColor = .orange
         lbl.textAlignment = .center
+        lbl.font = font
         self.view.addSubview(lbl)
         
-        btn.frame = CGRect.init(x: 140, y: 500, width: 150, height: 30)
+        btn.frame = CGRect.init(x: 140, y: 500, width: 150, height: 40)
         btn.setTitle("Click me", for: UIControl.State.normal)
         btn.addTarget(self, action: #selector(completed), for: UIControl.Event.touchDown)
         btn.backgroundColor = .lightText
         btn.setTitleColor(.magenta, for: UIControl.State.normal)
+        btn.titleLabel?.font = font
         self.view.addSubview(btn)
         
     }
