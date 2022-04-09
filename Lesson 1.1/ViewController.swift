9e7081b53c412a56cfd9664901add0ad8952d2e3
Bunyad <the.bunyad@gmail.com>
2022-04-09 18:20:31 +0400

    button added

--- Lesson 1.1/ViewController.swift
+++ Lesson 1.1/ViewController.swift
@@ -8,20 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let lbl = UILabel()
+    let btn = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         self.view.backgroundColor = .darkGray
-        let lbl = UILabel()
+        
+       
         lbl.frame = CGRect.init(x: 140, y: 400, width: 150, height: 30)
         lbl.text = "Testing.."
         lbl.textColor = .orange
         lbl.textAlignment = .center
         self.view.addSubview(lbl)
+        
+        btn.frame = CGRect.init(x: 140, y: 500, width: 150, height: 30)
+        btn.setTitle("Click me", for: UIControl.State.normal)
+        btn.addTarget(self, action: #selector(completed), for: UIControl.Event.touchDown)
+        btn.backgroundColor = .lightText
+        btn.setTitleColor(.magenta, for: UIControl.State.normal)
+        self.view.addSubview(btn)
+        
     }
-
+    
+    @objc func completed () {
+        lbl.text = "Test Completed!"
+    }
 
 }
 
