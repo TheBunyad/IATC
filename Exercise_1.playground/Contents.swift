2fd76b764450948855dc41dde1b84136e28ff2f0
Bunyad <the.bunyad@gmail.com>
2022-04-12 18:02:58 +0400

    .

---
+++ Exercise_1.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+import Darwin
+
+//Task 1
+print("Sherif")
+
+//Task 2
+print("Bunyad\nYasamal ray. M.A.Sherifzade")
+
+//Task 3
+var num = Int.random(in: 10...20) / 4
+print(num)
+
+//task 4
+var X = Int.random(in: 0...100)
+if X > 22 {print(X)}
+if X < 22 {print(X)}
+print(X%2)
+
+//Task 5
+var x = Int.random(in: 0...100)
+var y = Int.random(in: 0...100)
+var z = Int.random(in: 0...100)
+print(x + y + z)
